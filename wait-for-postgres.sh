89a25d1f5270a45a94a7ab4542e590ec4c71ffea
Dmitry Zeldin <dmitry@zeldin.pro>
2017-12-20 20:03:16 +0100

    Added wait-for-postgres.sh, as described in https://docs.docker.com/compose/startup-order/

---
+++ wait-for-postgres.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Taken from https://docs.docker.com/compose/startup-order/
+
+set -e
+
+cmd="$@"
+
+until psql -h $POSTGRES_HOST -U $POSTGRES_USER -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
